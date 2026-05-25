a76e86c7d3a16623cfd75131c008bc07752b2d5f
elianrocco <elianrocco03@gmail.com>
2026-05-25 03:39:40 -0300

    messge_queue_v0.1

--- SistemaRenderizadoNube/job.h
+++ SistemaRenderizadoNube/job.h
@@ -1,6 +1,11 @@
 #ifndef JOB_H_INCLUDED
 #define JOB_H_INCLUDED
 
+struct Job{};
+/*
+  -no encapsular
 
+  -agregar el tiempo en el que se crea como atributo de nombre "creacion"
+*/
 
 #endif // JOB_H_INCLUDED
--- SistemaRenderizadoNube/queue.h
+++
@@ -1,6 +0,0 @@
-#ifndef QUEUE_H_INCLUDED
-#define QUEUE_H_INCLUDED
-
-
-
-#endif // QUEUE_H_INCLUDED
