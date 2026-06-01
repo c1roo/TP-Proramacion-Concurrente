477433af45307b22506223a24bfa0549b0ba0491
Elias Ricarte <elias.ricarte@gmail.com>
2026-06-01 20:32:29 -0300

    añadido pool vram base

--- SistemaRenderizadoNube/pool_vram.h
+++ SistemaRenderizadoNube/pool_vram.h
@@ -1,6 +1,18 @@
 #ifndef POOL_VRAM_H_INCLUDED
 #define POOL_VRAM_H_INCLUDED
 
+#include <mutex>
+#include <condition_variable>
+#include "job.h"
 
+struct PoolVRAM {
+    std::mutex mtx;                          
+    std::condition_variable cv_disponible;   
+    int slots_ocupados = 0;
+    const int MAX_SLOTS = 5;                 
+};
+
+void asignarAVRAM(PoolVRAM& pool, Job& job);
+void liberarDeVRAM(PoolVRAM& pool, Job& job);
 
 #endif // POOL_VRAM_H_INCLUDED
