7fd3a105754f5c8e175d86bb1b659868e52e8de0
Elias Ricarte <elias.ricarte@gmail.com>
2026-06-03 19:52:33 -0300

    añadi buffer de verdad sin solo el contador

--- SistemaRenderizadoNube/pool_vram.cpp
+++ SistemaRenderizadoNube/pool_vram.cpp
@@ -2,59 +2,53 @@
 #include "log.h"
 #include <thread>
 #include <chrono>
-using namespace std;
 
 void init(PoolVRAM& pool, int n) {
-    pool.contador = n;
+    pool.capacidad_maxima = n;
+    pool.cantidad_activos = 0;
+    Job jobVacio;
+    jobVacio.id = 0;
+    pool.buffer_mensajes.assign(n, jobVacio);
 }
 
-void wait(PoolVRAM& pool) {
-    unique_lock<mutex> lock(pool.mtx);
-    while (pool.contador == 0) {
-        pool.cv.wait(lock);
+void wait(PoolVRAM& pool, const Job& job) {
+    std::unique_lock<std::mutex> lock(pool.mtx);
+    while (pool.cantidad_activos == pool.capacidad_maxima) {
+        pool.cv_espacio.wait(lock);
     }
-    pool.contador--;
+    for (int i = 0; i < pool.capacidad_maxima; ++i) {
+        if (pool.buffer_mensajes[i].id == 0) {
+            pool.buffer_mensajes[i] = job;
+            break;
+        }
+    }
+    pool.cantidad_activos++;
 }
 
-void signal(PoolVRAM& pool) {
-    unique_lock<mutex> lock(pool.mtx);
-    pool.contador++;
-    pool.cv.notify_one();
+void signal(PoolVRAM& pool, int jobId) {
+    std::unique_lock<std::mutex> lock(pool.mtx);
+    for (int i = 0; i < pool.capacidad_maxima; ++i) {
+        if (pool.buffer_mensajes[i].id == jobId) {
+            pool.buffer_mensajes[i].id = 0;
+            break;
+        }
+    }
+    pool.cantidad_activos--;
+    lock.unlock();
+    pool.cv_espacio.notify_one();
 }
 
 void asignarAVRAM(PoolVRAM& pool, Job& job) {
-    wait(pool);
-
-    this_thread::sleep_for(chrono::milliseconds(450));
-    string prio;
-    if(job.prioridad == 1)
-    {
-        prio = "Premium";
-    }
-    else 
-    {
-        prio = "Free";
-    }
-
+    wait(pool, job);
+    std::this_thread::sleep_for(std::chrono::milliseconds(450)); 
+    std::string prio = (job.prioridad == 1) ? "Premium" : "Free";
     registrarEvento(job.id, prio, "ASIGNADO_VRAM");
-
 }
 
 void liberarDeVRAM(PoolVRAM& pool, Job& job) {
-    this_thread::sleep_for(chrono::milliseconds(600));
-    signal(pool);
-    this_thread::sleep_for(chrono::milliseconds(250));
-
-    string prio;
-    if(job.prioridad == 1)
-    {
-        prio = "Premium";
-    }
-    else 
-    {
-        prio = "Free";
-    }
-
+    std::this_thread::sleep_for(std::chrono::milliseconds(600)); 
+    signal(pool, job.id);
+    std::this_thread::sleep_for(std::chrono::milliseconds(250)); 
+    std::string prio = (job.prioridad == 1) ? "Premium" : "Free";
     registrarEvento(job.id, prio, "FINALIZADO");
-
 }
