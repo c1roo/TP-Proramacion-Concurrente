f594164dc612743320f5b328e8a07056aaf1e265
elianrocco <elianrocco03@gmail.com>
2026-06-03 20:34:21 -0300

    Cambio de message queue 2

--- SistemaRenderizadoNube/message_queue.cpp
+++ SistemaRenderizadoNube/message_queue.cpp
@@ -1,29 +1,30 @@
 #include "message_queue.h"
+using namespace std;
 
 bool compararJobs(const Job& job1, const Job& job2) {
     return job1.prioridad < job2.prioridad; 
 }
 
 void addJob(MessageQueue& messageQueue, Job& job) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    std::lock_guard<std::mutex> lock(messageQueue.mtx);
+    this_thread::sleep_for(chrono::milliseconds(100));
+    lock_guard<mutex> lock(messageQueue.mtx);
     
     messageQueue.jobQueue.push(job);
 }
 
 
 Job getJob(MessageQueue& messageQueue) {
-    std::vector<Job> vectorAux;
+    vector<Job> vectorAux;
 
-    std::lock_guard<std::mutex> lock(messageQueue.mtx);
+    lock_guard<mutex> lock(messageQueue.mtx);
     while (!messageQueue.jobQueue.empty()) {
         vectorAux.push_back(messageQueue.jobQueue.top());
         messageQueue.jobQueue.pop();
     }
 
     for (Job& j : vectorAux) {
-        auto ahora = std::chrono::high_resolution_clock::now();
-        auto tiempoDeEspera = std::chrono::duration_cast<std::chrono::milliseconds>(ahora - j.creacion);
+        auto ahora = chrono::high_resolution_clock::now();
+        auto tiempoDeEspera = chrono::duration_cast<chrono::milliseconds>(ahora - j.creacion);
 
         if (tiempoDeEspera.count() >= 5000) {
             j.prioridad = (int)(tiempoDeEspera.count() / 2500);
